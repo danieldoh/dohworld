1f709377755a3e4730f1a46cf6a495173bde4e6f
danieldoh <doh7337@gmail.com>
2024-07-12 20:17:03 +0900

    Applied map for the research interest contents and changed the section name into interest

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,8 +4,8 @@ export const headerNav = [
         url: "#intro",
     },
     {
-        title: "skill",
-        url: "#skill",
+        title: "interest",
+        url: "#interest",
     },
     {
         title: "site",
@@ -25,3 +25,14 @@ export const introText = {
     title: "Hyungjun Doh",
     desc: ["MS in Electrical and Computer Engineering", "Purdue University"],
 };
+
+export const interestText = [
+    {
+        title: "Aritifical Intelligence",
+        desc: "I am an undergraduate student in the Elmore Family School of Electrical and Computer Engineering at Purdue University. I am a member of the Convergence Design Lab, where I work as an undergraduate research assistant. My research interests are Human-AI interaction and its practical applications, with a specific focus on Extended Reality, Task Guidance Systems, and AI-infused interfaces.",
+    },
+    {
+        title: "Human-Computer Interaction",
+        desc: "Human-AI Interaction.",
+    },
+];
